9ce735187d0403a0d879d165cd7247f6daee0bc9
Gilles Chanteau <gilecad@orange.fr>
2024-03-26 22:51:26 +0100

    Delete AutoCAD Fsharp Plugin Template directory

--- AutoCAD Fsharp Plugin Template/Script.fsx
+++
@@ -1,8 +0,0 @@
-﻿// Learn more about F# at https://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-#load "Commands.fs"
-open $safeprojectname$
-
-// Define your library scripting code here
-
